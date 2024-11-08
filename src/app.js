4a0905840b76bfcde5ed101357504f3dda6bf953
josemartinbm99 <josemartinbm99@gmail.com>
2024-11-08 08:23:13 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomWho() {
+  const tipesOfName = new Array("Carlos", "Hans", "Un alemán");
+  let rnd = Math.random();
+  const randomPositionOfName = Math.floor(rnd * tipesOfName.length);
+  return tipesOfName[randomPositionOfName];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getRandomAction() {
+  const tipesOfName = new Array("se comió", "quemó", "vendió");
+  let rnd = Math.random();
+  const randomPositionOfName = Math.floor(rnd * tipesOfName.length);
+  return tipesOfName[randomPositionOfName];
+}
+
+function getRandomWhat() {
+  const tipesOfName = new Array("mi ordenador", "España", "mi casa");
+  let rnd = Math.random();
+  const randomPositionOfName = Math.floor(rnd * tipesOfName.length);
+  return tipesOfName[randomPositionOfName];
+}
+function getRandomWhen() {
+  const tipesOfName = new Array("hoy", "ayer", "no sé cuando");
+  let rnd = Math.random();
+  const randomPositionOfName = Math.floor(rnd * tipesOfName.length);
+  return tipesOfName[randomPositionOfName];
+}
+
+function excuseGeneratorNimbus2000() {
+  const who = getRandomWho();
+  const action = getRandomAction();
+  const what = getRandomWhat();
+  const when = getRandomWhen();
+  return who + " " + action + " " + what + " " + when;
+}
+document.getElementById(
+  "excuseGeneratorNimbus2000"
+).innerHTML = excuseGeneratorNimbus2000();
